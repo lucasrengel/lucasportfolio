4f9cd4e6416d7dec87782fbdf01dfd86e3afe190
lucasrengel <83765048+lucasrengel@users.noreply.github.com>
2021-07-16 09:37:51 -0300

    Update questao2.sh

--- FundamentosTI/exemplos/questao2.sh
+++ FundamentosTI/exemplos/questao2.sh
@@ -3,4 +3,6 @@
 cat "2 a 4 sao as letras de informaçoes de usuario
 r = ler w = escrever e x = executar"
 
-echo "qual arquivo quer o perssionamento" read $1 ls -l $1
+echo "qual arquivo quer o perssionamento"
+read $1 
+ls -l $1
