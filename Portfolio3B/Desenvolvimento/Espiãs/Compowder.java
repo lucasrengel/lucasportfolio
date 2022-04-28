83700b69b8f76f23432535bd08516bfa87d51c7b
luvas <83765048+lucasrengel@users.noreply.github.com>
2022-04-28 10:47:28 -0300

    Create Compowder.java

---
+++ Portfolio3B/Desenvolvimento/Espiãs/Compowder.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class Compowder{
+  private int serial;
+  private String nomeEspia;
+  private float versao;
+  private ArrayList<String> funcoes;
+
+public Compowder(int serial, String nomeEspia, float versao){ 
+  this.serial = serial;
+  this.nomeEspia = nomeEspia;
+  this.versao = versao;
+  }
+
+public void rastrear(){
+  }
+
+public void usarCamera(){
+  }
+
+public void ligarChamada(){
+  }
+
+public void lerMusica(){
+  }
+
+public boolean analizarVoz(){
+  return true;
+  }
+
+public void gravarImagem(){
+  }
+
+public void conectarDispositivo(){
+  }
+
+public void mudarDeRoupa(){
+  }
+
+public String mudarAparencia(String nomeEspia, String mudarRoupa){
+  return mudarRoupa;
+  }
+
+public void ligarLaser(){
+  }
+
+public boolean usarSecador(){
+  return true;
+  }
+
+public String escanearImpressao(String smartphone, String gravacaoImagem, int conexaoOutrosDispositivos){
+  return gravacaoImagem;
+  }
+
+public void projetarHolograma(){
+  }
+
+public void projetarImagem(){
+  }
+
+public boolean monitorarBatimentos(float monitoramentoSinaisBiologicos, int acessarSistemacomputador, String projecaoImagens){
+  return true;
+  }
+
+public void ativarGPS(){
+  }
+
+public void acessarComputador(){
+  }
+
+public void paginar(){
+  }
+}
+
