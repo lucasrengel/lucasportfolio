2a4f982942c99a2442de8f64bc52c6e848dc308d
luvas <83765048+lucasrengel@users.noreply.github.com>
2022-04-28 10:47:06 -0300

    Create Aparelhos.java

---
+++ Portfolio3B/Desenvolvimento/Espiãs/Aparelhos.java
@@ -0,0 +1,55 @@
+public class Aparelhos{
+  private String cicloAntigravitacional;
+  private String armaCongelante;
+  private String macacao;
+  private int po;
+  private String evapoblaster;
+  private int sistemaMisseis;
+  private int pastilhaElastica;
+  private String helicoptero;
+  private String imobilizadoraBronzeada;
+  private String mochilaJato;
+  private String revolverLaser;
+  private int cartaoDeCredito;
+  private String fioNavalha;
+  private float relogioScanner;
+  private String motoNeve;
+  private String trajeEspacial;
+  private String tornadoLata;
+
+public Aparelhos(String cicloAntigravitacional, String armaCongelante, String macacao, int po, String evapoblaster, int sistemaMisseis, int pastilhaElastica, String helicoptero, String imobilizadoraBronzeada, String mochilaJato, String revolverLaser, int cartaoDeCredito, String fioNavalha, float relogioScanner, String motoNeve, String trajeEspacial, String tornadoLata){
+  this.cicloAntigravitacional = cicloAntigravitacional;
+  this.armaCongelante = armaCongelante;
+  this.macacao = macacao;
+  this.po = po;
+  this.evapoblaster = evapoblaster;
+  this.sistemaMisseis = sistemaMisseis;
+  this.pastilhaElastica = pastilhaElastica;
+  this.helicoptero = helicoptero;
+  this.imobilizadoraBronzeada = imobilizadoraBronzeada;
+  this.mochilaJato = mochilaJato;
+  this.revolverLaser = revolverLaser;
+  this.cartaoDeCredito = cartaoDeCredito;
+  this.fioNavalha = fioNavalha;
+  this.relogioScanner = relogioScanner;
+  this.motoNeve = motoNeve;
+  this.trajeEspacial = trajeEspacial;
+  this.tornadoLata = tornadoLata;
+  }
+
+public void verificarAparelhos(){
+  }
+  
+public void verificarMunicoes(){
+  }
+
+public void verificarAtualizacoes(){
+  }
+
+public void testarNovosExperimentos(){
+  }
+
+public void guardarDesnecessarios(){
+  }
+
+}
